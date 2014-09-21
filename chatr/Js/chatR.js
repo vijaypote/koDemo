84548bf47a106f6295bc7d30bb6c2d7cecd0b75e
Vijay Pote <vijay.pots@gmail.com>
2014-09-21 09:53:08 -0700

    added for loop

--- chatr/Js/chatR.js
+++ chatr/Js/chatR.js
@@ -1,11 +1,4 @@
-﻿
-/*
- * Author: Valerio Gheri
- * Date: 22/07/2012
- * Description: ChatR namespace js file and viewmodels declaration
- */
-
-// Namespace
+﻿// Namespace
 var chatR = {};
 
 // Models
@@ -14,14 +7,14 @@ chatR.chatMessage = function (id, parentid, sender, content, dateSent, chatMessa
     var self = this;
     //alert(parentid);
     self.id = id;
-    self.parentId = parentid;    
+    self.parentId = parentid;
     self.chatMessageSender = chatMessageSender;
 
     self.username = sender;
     self.content = content;
     if (dateSent != null) {
         self.timestamp = dateSent;
-    }    
+    }
 
     self.newMessage = ko.observable("");
     self.doReply = ko.observable(false);
@@ -55,6 +48,7 @@ chatR.user = function (username, userId) {
 // ViewModels
 
 chatR.chatViewModel = function (usersModel, currentUser, chatMessageSender) {
+
     var self = this;
 
     self.chatMessageSender = chatMessageSender;
@@ -65,7 +59,7 @@ chatR.chatViewModel = function (usersModel, currentUser, chatMessageSender) {
     self.parentMessage = ko.observable('');
 
     self.sendMessage = function () {
-        var msg = { parentId: 0, username: self.currentUser.username, content: self.parentMessage() };        
+        var msg = { parentId: 0, username: self.currentUser.username, content: self.parentMessage() };
         self.chatMessageSender(msg);
         self.parentMessage('');
     };
@@ -85,7 +79,7 @@ chatR.chatViewModel = function (usersModel, currentUser, chatMessageSender) {
             if (parentMessage != null) {
                 parentMessage.replies.push(newMessage);
                 break;
-            } 
+            }
         }
     };
 
@@ -108,7 +102,7 @@ chatR.chatViewModel = function (usersModel, currentUser, chatMessageSender) {
             if (parentMessage != null) {
                 return parentMessage;
             }
-            
+
         }
 
         //for (var k = 0; k < message.replies().length; k++) {
@@ -120,179 +114,98 @@ chatR.chatViewModel = function (usersModel, currentUser, chatMessageSender) {
 
     self.usersModel = usersModel;
 
+
     // load data in begining
-    var fakeData = [{
-        "messages": [
-          {
-              "id": 1,
-              "parentId": 0,
-              "username": "vap",
-              "content": "Who are you ?",
-              "timestamp": "2014-09-12T06:06:41.848Z",
-              "newMessage": "",
-              "doReply": false,
-              "replies": [
-                {
-                    "id": 2,
-                    "parentId": 1,
-                    "username": "vap",
-                    "content": "I am Vijay",
-                    "timestamp": "2014-09-12T06:06:51.591Z",
-                    "newMessage": "",
-                    "doReply": false,
-                    "replies": [
-                      {
-                          "id": 7,
-                          "parentId": 2,
-                          "username": "vap",
-                          "content": "I am vijay pote",
-                          "timestamp": "2014-09-12T06:08:20.920Z",
-                          "newMessage": "",
-                          "doReply": false,
-                          "replies": []
-                      }
-                    ]
-                },
-                {
-                    "id": 3,
-                    "parentId": 1,
-                    "username": "vap",
-                    "content": "I am Shetty",
-                    "timestamp": "2014-09-12T06:07:11.836Z",
-                    "newMessage": "",
-                    "doReply": false,
-                    "replies": [
-                      {
-                          "id": 8,
-                          "parentId": 3,
-                          "username": "vap",
-                          "content": "I am prashant shetty",
-                          "timestamp": "2014-09-12T06:08:43.543Z",
-                          "newMessage": "",
-                          "doReply": false,
-                          "replies": []
-                      }
-                    ]
-                },
-                {
-                    "id": 4,
-                    "parentId": 1,
-                    "username": "vap",
-                    "content": "I am Jayesh",
-                    "timestamp": "2014-09-12T06:07:32.438Z",
-                    "newMessage": "",
-                    "doReply": false,
-                    "replies": [
-                      {
-                          "id": 9,
-                          "parentId": 4,
-                          "username": "vap",
-                          "content": "i am jayesh yeragi",
-                          "timestamp": "2014-09-12T06:08:54.479Z",
-                          "newMessage": "",
-                          "doReply": false,
-                          "replies": []
-                      },
-                      {
-                          "id": 10,
-                          "parentId": 4,
-                          "username": "vap",
-                          "content": "okay got it",
-                          "timestamp": "2014-09-12T06:09:10.995Z",
-                          "newMessage": "",
-                          "doReply": false,
-                          "replies": []
-                      }
-                    ]
+    var fakeData = '[{"messages": [{"id": 1,"parentId": 0,"username": "vap","content": "Who are you ?","timestamp": "2014-09-12T06:06:41.848Z","newMessage": "","doReply": false,"replies": [{"id": 2,"parentId": 1,"username": "vap","content": "I am Vijay","timestamp": "2014-09-12T06:06:51.591Z","newMessage": "","doReply": false,"replies": [{"id": 7,"parentId": 2,"username": "vap","content": "I am vijay pote","timestamp": "2014-09-12T06:08:20.920Z","newMessage": "","doReply": false,"replies": []}]},{"id": 3,"parentId": 1,"username": "vap","content": "I am Shetty","timestamp": "2014-09-12T06:07:11.836Z","newMessage": "","doReply": false,"replies": [{"id": 8,"parentId": 3,"username": "vap","content": "I am prashant shetty","timestamp": "2014-09-12T06:08:43.543Z","newMessage": "","doReply": false,"replies": []}]},{"id": 4,"parentId": 1,"username": "vap","content": "I am Jayesh","timestamp": "2014-09-12T06:07:32.438Z","newMessage": "","doReply": false,"replies": [{"id": 9,"parentId": 4,"username": "vap","content": "i am jayesh yeragi","timestamp": "2014-09-12T06:08:54.479Z","newMessage": "","doReply": false,"replies": []},{"id": 10,"parentId": 4,"username": "vap","content": "okay got it","timestamp": "2014-09-12T06:09:10.995Z","newMessage": "","doReply": false,"replies": []}]}]},{"id": 5,"parentId": 0,"username": "vap","content": "Where are you ?","timestamp": "2014-09-12T06:07:58.861Z","newMessage": "","doReply": false,"replies": [{"id": 6,"parentId": 5,"username": "vap","content": "I am right here","timestamp": "2014-09-12T06:08:07.020Z","newMessage": "","doReply": false,"replies": []}]}],"currentUser": {"username": "vap"},"parentMessage": "","usersModel": {"contacts": [{"username": "vap","id": "2ce3aa2b-629b-4bc2-bace-ef6931dfc2eb"}]}}]';
+
+    //Recursive loop
+    //iterate(fakeData);
+
+    var json = $.parseJSON(fakeData);
+
+    // get all message count
+    var allMessage = json[0].messages;
+    var allMessageCnt = json[0].messages.length;
+    var loadCurrentUser = json[0].currentUser;
+
+    self.currentUser = loadCurrentUser;
+    var messageReplies = null;
+    var messageRepliesCnt = null;
+    var messageInnerRepliesCnt = null;
+
+    $.each(json, function (idx, obj) {
+        for (i = 0, l = allMessageCnt; i < l; i++) {
+            //alert(json[idx].messages[i].id);
+            //alert(json[idx].messages[i].parentId);
+            //alert(json[idx].messages[i].username);
+            //alert(json[idx].messages[i].content);
+            //alert(json[idx].messages[i].timestamp);
+
+            //self.messages.id = json[idx].messages[i].id;
+            //self.messages.parentId = json[idx].messages[i].parentId;
+            //self.messages.username = json[idx].messages[i].username;
+            //self.messages.content = json[idx].messages[i].content;
+            //self.messages.timestamp = json[idx].messages[i].timestamp;
+
+            self.addMessage(new chatR.chatMessage(json[idx].messages[i].id, json[idx].messages[i].parentId, json[idx].messages[i].username, json[idx].messages[i].content, json[idx].messages[i].timestamp, self.chatMessageSender));
+
+
+            //messageReplies = json[idx].messages[i].replies[i];
+            messageNextRepliesCnt = json[idx].messages[i].replies.length;
+
+            for (j = 0, m = messageNextRepliesCnt; j < m; j++) {
+                //alert(json[idx].messages[i].replies[j].id);
+                //alert(json[idx].messages[i].replies[j].parentId);
+                //alert(json[idx].messages[i].replies[j].username);
+                //alert(json[idx].messages[i].replies[j].content);
+                //alert(json[idx].messages[i].replies[j].timestamp);                
+
+
+                self.addReply(new chatR.chatMessage(json[idx].messages[i].replies[j].id, json[idx].messages[i].replies[j].parentId, json[idx].messages[i].replies[j].username, json[idx].messages[i].replies[j].content, json[idx].messages[i].replies[j].timestamp, self.chatMessageSender));
+
+                messageInnerRepliesCnt = json[idx].messages[i].replies[j].replies.length;
+
+                for (k = 0, n = messageInnerRepliesCnt; k < n; k++) {
+                    //alert(json[idx].messages[i].replies[j].replies[k].id);
+                    //alert(json[idx].messages[i].replies[j].replies[k].parentId);
+                    //alert(json[idx].messages[i].replies[j].replies[k].username);
+                    //alert(json[idx].messages[i].replies[j].replies[k].content);
+                    //alert(json[idx].messages[i].replies[j].replies[k].timestamp);
+
+                    //messageInnerRepliesCnt = json[idx].messages[i].replies[j].replies.length;
+
                 }
-              ]
-          },
-          {
-              "id": 5,
-              "parentId": 0,
-              "username": "vap",
-              "content": "Where are you ?",
-              "timestamp": "2014-09-12T06:07:58.861Z",
-              "newMessage": "",
-              "doReply": false,
-              "replies": [
-                {
-                    "id": 6,
-                    "parentId": 5,
-                    "username": "vap",
-                    "content": "I am right here",
-                    "timestamp": "2014-09-12T06:08:07.020Z",
-                    "newMessage": "",
-                    "doReply": false,
-                    "replies": []
-                }
-              ]
-          }
-        ],
-        "currentUser": {
-            "username": "vap"
-        },
-        "parentMessage": "",
-        "usersModel": {
-            "contacts": [
-              {
-                  "username": "vap",
-                  "id": "2ce3aa2b-629b-4bc2-bace-ef6931dfc2eb"
-              }
-            ]
+            }
+            //alert("---------------------------------------------------------------------------------");
         }
-    }];
+        //alert(json[idx].messages[idx].id);
+    });
 
-    //alert(ko.mapping.fromJS(fakeData[0]))
+    //var allContacts = obj[0].usersModel.contacts;
 
-    //self.messages = ko.mapping.fromJS(fakeData[0]);
-    //self.replies = ko.mapping.fromJS(fakeData[0]);
-    //self.user = ko.mapping.fromJS(fakeData[1]);
-    //self.parentMessage = ko.mapping.fromJS(fakeData[2]);
-    //self.usersModel = ko.mapping.fromJS(fakeData[3]);
-    self.addReply = "";
-
-    //self.sendMessage = function () {
-
-    //};
-
-    self.replyNow = function () {
-        //self.doReply(true);
-        //console.log('Replyt now');
-    };
-
-    chatR.chatViewModel = ko.mapping.fromJS(fakeData[0]);
-    chatR.user = ko.mapping.fromJS(fakeData[1]);
-    chatR.parentMessage = ko.mapping.fromJS(fakeData[2]);
-    chatR.usersModel = ko.mapping.fromJS(fakeData[3]);
-    //chatR.chatViewModel = self;
-
-    ko.applyBindings(chatR.chatViewModel);
-
-    //ko.applyBindings(chatR.chatViewModel.userModel.contacts);
-    //alert(json);
-    //self.messages = json.messages;
-
-    //var mappedTasks = $.map(json, function (item) {
-    //    console.log(item);
-    //});
-
-    //chat.loadJson();
-    //ko.applyBindings(self.loadJson, document.getElementById("myContainer"));
-    //var obj = JSON.parse(ko.toJSON(fakeData));
-    //var objJSON = ko.toJSON(fakeData);
+    //self.usersModel = allContacts[0];
 
 
-    //alert(fakeData.messages);
-    //var mappedTasks = $.map(fakejSon, function (item) {
-    //    alert(item);
-    //});
 
-    //self.messages(fakeData.messages);    
-    //self.currentUser(fakejSon.currentUser.username);
 
     return self;
 }
+
+function iterate(obj) {
+    for (var key in obj) { // iterate, `key` is the property key
+        var elem = obj[key]; // `obj[key]` is the value
+
+        if (key === "content") { // found "text" property
+            alert(elem)
+        }
+
+        if (typeof elem === "object") { // is an object (plain object or array),
+            // so contains children
+            iterate(elem); // call recursively
+        }
+    }
+}
+
 
 chatR.connectedUsersViewModel = function () {
     var self = this;
